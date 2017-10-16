dac8a63a7d6dbedf68939e2304c20e82c76bc05f
eneskaya <eneskaya@me.com>
2017-10-16 14:34:02 +0200

    Queue impl and tests

--- schlange.erl
+++ schlange.erl
@@ -1,1 +1,30 @@
 -module (schlange).
+-export ([createQ/0, enqueue/2, dequeue/1, isEmptyQ/1,
+          equalQ/2, front/1]).
+
+createQ() ->
+  InStack = stack:createS(),
+  OutStack = stack:createS(),
+  {InStack, OutStack}.
+
+enqueue({In, Out}, Element) ->
+  NewIn = stack:push(In, Element),
+  {NewIn, Out}.
+
+dequeue({{}, Out}) -> {stack:createS(), stack:pop(Out)};
+dequeue({In, {}}) -> {stack:createS(), stack:pop(stack:reverseS(In))};
+dequeue({{}, {}}) -> createQ().
+
+isEmptyQ({In, Out}) ->
+  InIsEmpty = stack:isEmptyS(In),
+  OutIsEmpty = stack:isEmptyS(Out),
+  InIsEmpty and OutIsEmpty.
+
+equalQ({In1, Out1}, {In2, Out2}) ->
+  InStacksAreEqual = stack:equalS(In1, In2),
+  OutStacksAreEqual = stack:equalS(Out1, Out2),
+  InStacksAreEqual and OutStacksAreEqual.
+
+front({{}, Out}) -> stack:top(Out);
+front({In, {}}) -> stack:top(stack:reverseS(In));
+front({{}, {}}) -> throw(queue_empty).
--- schlange_tests.erl
+++ schlange_tests.erl
@@ -4,3 +4,28 @@
 createQ_test() ->
   Q = schlange:createQ(),
   ?assert(schlange:isEmptyQ(Q)).
+
+enqueue_test() ->
+  Q = schlange:createQ(),
+  Q1 = schlange:enqueue(Q, 1),
+  Q2 = schlange:enqueue(Q1, 2),
+  Front = schlange:front(Q2),
+  ?assert(not schlange:isEmptyQ(Q2)),
+  ?assertEqual(1, Front).
+
+equalQ_test() ->
+  Q = schlange:createQ(),
+  ?assert(schlange:equalQ(Q, Q)),
+  Q1 = schlange:enqueue(Q, 1),
+  Q2 = schlange:enqueue(Q1, 2),
+  Q3 = schlange:enqueue(Q1, 3),
+  ?assertNot(schlange:equalQ(Q1, Q2)),
+  ?assertNot(schlange:equalQ(Q2, Q3)).
+
+front_test() ->
+  Q = schlange:createQ(),
+  Q1 = schlange:enqueue(Q, 1),
+  Q2 = schlange:enqueue(Q1, 2),
+  Q3 = schlange:dequeue(Q2),
+  Front = schlange:front(Q3),
+  ?assertEqual(2, Front).
