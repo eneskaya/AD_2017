18057a31a2ea046a914d4ea3b8e13c5d2ec81b54
eneskaya <eneskaya@me.com>
2017-12-12 09:13:26 +0100

    Experiments

--- experimente.erl
+++ experimente.erl
@@ -1,6 +1,7 @@
 -module(experimente).
 -export([start/0, startBT/0]).
 
+-import(math, [pow/2]).
 -import(avl_utils, [insertRek/2, resetCounters/0, 
                     getCountersTuple/0, printCounters/0]).
 
@@ -23,11 +24,19 @@ startWithCount(Count) ->
 
 start() ->
     io:fwrite("Starte Test für AVL-Baum: ~n"),
-    startWithCount(1000),
-    startWithCount(10000),
-    startWithCount(100000),
-    startWithCount(1000000),
-    startWithCount(10000000).
+    startWithCount(trunc(math:pow(2,10))),
+    startWithCount(trunc(math:pow(2,12))),
+    startWithCount(trunc(math:pow(2,14))),
+    startWithCount(trunc(math:pow(2,16))),
+    startWithCount(trunc(math:pow(2,18))),
+    startWithCount(trunc(math:pow(2,20))),
+    startWithCount(trunc(math:pow(2,22))),
+    startWithCount(trunc(math:pow(2,24))),
+    startWithCount(trunc(math:pow(2,26))),
+    startWithCount(trunc(math:pow(2,28))),
+    startWithCount(trunc(math:pow(2,30))),
+    startWithCount(trunc(math:pow(2,32))).
+
 
 insertRekBTree(B, []) -> B;
 insertRekBTree(B, [H|T]) -> insertRek(btree:insertBT(B, H), T).
@@ -43,13 +52,20 @@ startBTWithCount(Count) ->
     io:fwrite("Laufzeit für Einfügen von ~p Elementen: ~ps ~n", [ Count, Delta ]).
 
 startBT() ->
-    startBTWithCount(1000),
-    startBTWithCount(10000),
-    startBTWithCount(100000),
-    startBTWithCount(1000000),
-    startBTWithCount(10000000).
+    startBTWithCount(trunc(math:pow(2,10))),
+    startBTWithCount(trunc(math:pow(2,12))),
+    startBTWithCount(trunc(math:pow(2,14))),
+    startBTWithCount(trunc(math:pow(2,16))),
+    startBTWithCount(trunc(math:pow(2,18))),
+    startBTWithCount(trunc(math:pow(2,20))),
+    startBTWithCount(trunc(math:pow(2,22))),
+    startBTWithCount(trunc(math:pow(2,24))),
+    startBTWithCount(trunc(math:pow(2,26))),
+    startBTWithCount(trunc(math:pow(2,28))),
+    startBTWithCount(trunc(math:pow(2,30))),
+    startBTWithCount(trunc(math:pow(2,32))).
 
-klcTest(Count) ->
-    List = util:randomliste(Count),
-    B = avltree:initBT(),
+% klcTest(Count) ->
+%     List = util:randomliste(Count),
+%     B = avltree:initBT(),
     
